fde6d04031bb4bea4347a28ac5ac3c4145317c93
He-Man <168465470+Kratosthegreat@users.noreply.github.com>
2025-07-02 00:48:22 +0300

    Update maya_bot_script.py

--- maya_bot_script.py
+++ maya_bot_script.py
@@ -11,7 +11,7 @@ import google.generativeai as genai
 
 # הגדרות
 
-TELEGRAM_TOKEN = os.getenv(“TELEGRAM_TOKEN”, “7876544988:AAFZUHIzHOqyzpJ5TIec2hJFtdiawc4JMF4”)
+TELEGRAM_TOKEN = “7876544988:AAFZUHIzHOqyzpJ5TIec2hJFtdiawc4JMF4”
 GEMINI_API_KEY = “AIzaSyBoIvgf3WlDQj1gDfGySUOi_JXqR-8GdcM”
 
 # הגדרת Gemini
